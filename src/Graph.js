070c79a35ef3ebfe9049bc4300213453fb20f847
Kapakahi <rubbishman@gmail.com>
2020-04-11 20:07:49 +0900

    Completed addEdge in Graph

--- src/Graph.js
+++ src/Graph.js
@@ -7,6 +7,45 @@ class Graph {
      */
     this.nodes = {}; // Adjacency List
   }
+
+  addNode(value) {
+    this.nodes[value] = [];
+  }
+
+  removeNode(value) {
+    delete this.nodes[value];
+    // console.log(this.nodes, "asdfasdfasdfasdfasdF");
+    for (let key in this.nodes) {
+      for (let i = 0; i < this.nodes[key].length; i++) {
+        if (this.nodes[key][i] === value) {
+          this.nodes[key].splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  contains(value) {
+    let isFound = false;
+    for (let key in this.nodes) {
+      if (key === value.toString()) {
+        isFound = true;
+        break;
+      }
+    }
+    return isFound;
+  }
+
+  addEdge(value1, value2) {
+    if (!this.nodes[value1] || !this.nodes[value2]) {
+      return "Invalid node value";
+    }
+    console.log(this.nodes[value1], "asdasdfasdfad");
+    if (!this.nodes[value1].includes(value2)) {
+      this.nodes[value1].push(value2);
+      this.nodes[value2].push(value1);
+    }
+  }
 }
 
 /*
@@ -17,3 +56,5 @@ class Graph {
 |X                               X
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 */
+
+module.exports = Graph;
